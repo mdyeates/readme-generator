a73e977ccc15bad7441ef5cc1522a1cbdf1d6a57
Michael Yeates <michael-yeates@outlook.com>
2023-01-21 16:23:03 +0000

    Replace input type with editor for credit urls, re-format question numbers

--- index.js
+++ index.js
@@ -9,51 +9,51 @@ function questions() {
       {
         default: "John Doe",
         name: "full_name",
-        message: "1) Enter your first and last name:",
+        message: "1. Enter your first and last name:",
       },
       {
         default: "john.doe@gmail.com",
         name: "email",
-        message: "2) Enter your email address:",
+        message: "2. Enter your email address:",
         validate: (email) =>
           /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? true : "Please enter a valid email address",
       },
       {
         default: "johndoe",
         name: "github_username",
-        message: "3) Enter your GitHub username:",
+        message: "3. Enter your GitHub username:",
       },
       {
         default: "johndoe",
         name: "linkedin_username",
-        message: "4) Enter your LinkedIn username:",
+        message: "4. Enter your LinkedIn username:",
       },
       {
         default: "my-project",
         name: "repo_name",
-        message: "5) Enter the name of your GitHub repository:",
+        message: "5. Enter the name of your GitHub repository:",
       },
       {
         default: "My Cool Project",
         name: "title",
-        message: "6) Enter the title of your project:",
+        message: "6. Enter the title of your project:",
       },
       {
         default:
           "Experience the pinnacle of technology at your fingertips, as we empower you to effortlessly tackle every task and exceed all expectations, elevating your life to new heights of efficiency, convenience and brilliance.",
         name: "tagline",
-        message: "7) Provide a short and catchy tagline:",
+        message: "7. Provide a short and catchy tagline:",
       },
       {
         default:
           "Our application is designed to simplify and streamline your daily routine. With its user-friendly interface and cutting-edge technology, it offers a comprehensive set of features to enhance your productivity, organisation, and overall experience. Whether it's managing your schedule, organising your tasks, or connecting with loved ones, our application has you covered. It's available on multiple platforms and devices, so you can take it with you wherever you go. See the difference it makes in your life.",
         name: "description",
-        message: "8) Provide a detailed description of your project:",
+        message: "8. Provide a detailed description of your project:",
       },
       {
         type: "checkbox",
         name: "technologies",
-        message: "9) Select all technologies used in your project:",
+        message: "9. Select all technologies used in your project:",
         choices: [
           "HTML5",
           "CSS3",
@@ -79,17 +79,19 @@ function questions() {
       {
         default: "N/A",
         name: "tests",
-        message: "10) Provide information on how to run tests for your project:",
+        message: "10. Provide information on how to run tests for your project:",
       },
       {
-        default: ["https://jquery.com/", "https://getbootstrap.com/"],
+        type: "editor",
+        default: "https://google.com",
         name: "credits",
-        message: "11) Paste in the website URL's that you wish to acknowledge for their contributions to your project:",
+        message:
+          "11. Please insert the URLs you want to credit, one per line. (E.g Edit: press 'i', Save: press 'ESC' & type ':wq'):",
       },
       {
         type: "list",
         name: "license",
-        message: "12) Finally, select a license for your project:",
+        message: "12. Finally, select a license for your project:",
         choices: ["MIT", "Apache", "GNU GPLv3"],
       },
     ])
