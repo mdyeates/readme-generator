4760c0358d0d977e0f312dff446d6416611e4907
Michael Yeates <michael-yeates@outlook.com>
2023-01-21 03:50:41 +0000

    Add badges, validator checks, default values, required sections

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -8,56 +7,90 @@ function questions() {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "username",
-        message: "Enter your GitHub username:",
+        default: "John Doe",
+        name: "full_name",
+        message: "1) Enter your first and last name:",
       },
       {
-        type: "input",
-        name: "repoName",
-        message: "Enter your GitHub repository name:",
+        default: "john.doe@gmail.com",
+        name: "email",
+        message: "2) Enter your email address:",
+        validate: (email) =>
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) ? true : "Please enter a valid email address",
       },
       {
-        type: "input",
+        default: "johndoe",
+        name: "github_username",
+        message: "3) Enter your GitHub username:",
+      },
+      {
+        default: "johndoe",
+        name: "linkedin_username",
+        message: "4) Enter your LinkedIn username:",
+      },
+      {
+        default: "my-project",
+        name: "repo_name",
+        message: "5) Enter the name of your GitHub repository:",
+      },
+      {
+        default: "My Cool Project",
         name: "title",
-        message: "Enter your Project Title:",
+        message: "6) Enter the title of your project:",
       },
       {
-        type: "input",
-        name: "shortDescription",
-        message: "Provide a Short Description:",
+        default:
+          "Experience the pinnacle of technology at your fingertips, as we empower you to effortlessly tackle every task and exceed all expectations, elevating your life to new heights of efficiency, convenience and brilliance.",
+        name: "tagline",
+        message: "7) Provide a short and catchy tagline:",
       },
       {
-        type: "input",
-        name: "longDescription",
-        message: "Provide a Longer Description:",
+        default:
+          "Our application is designed to simplify and streamline your daily routine. With its user-friendly interface and cutting-edge technology, it offers a comprehensive set of features to enhance your productivity, organisation, and overall experience. Whether it's managing your schedule, organising your tasks, or connecting with loved ones, our application has you covered. It's available on multiple platforms and devices, so you can take it with you wherever you go. See the difference it makes in your life.",
+        name: "description",
+        message: "8) Provide a detailed description of your project:",
       },
       {
         type: "checkbox",
         name: "technologies",
-        message: "Select all the technologies used in your project:",
-        choices: ["jQuery", "Node.js", "React", "Bootstrap"],
+        message: "9) Select all technologies used in your project:",
+        choices: [
+          "HTML5",
+          "CSS3",
+          "JavaScript",
+          "jQuery",
+          "Node.js",
+          "React",
+          "Express.js",
+          "Bootstrap",
+          "Tailwind-CSS",
+          "Sass",
+          "MongoDB",
+          "Netlify",
+          "Heroku",
+          "PHP",
+          "Vue.js",
+          "Django",
+          "Laravel",
+          "MySQL",
+        ],
+        validate: (answer) => (answer.length === 0 ? "Please select at least one technology" : true),
       },
       {
-        type: "input",
-        name: "installation",
-        message: "Provide step by step installation instructions:",
+        default: "N/A",
+        name: "tests",
+        message: "10) Provide information on how to run tests for your project:",
       },
       {
-        type: "input",
+        default: ["https://jquery.com/", "https://getbootstrap.com/"],
         name: "credits",
-        message: "List your collaborators:",
+        message: "11) Paste in the website URL's that you wish to acknowledge for their contributions to your project:",
       },
       {
         type: "list",
         name: "license",
-        message: "Select your license:",
-        choices: ["MIT", "Apache", "None"],
-      },
-      {
-        type: "input",
-        name: "linkedInUsername",
-        message: "Enter your LinkedIn username:",
+        message: "12) Finally, select a license for your project:",
+        choices: ["MIT", "Apache", "GNU GPLv3"],
       },
     ])
     .then((responses) => {
@@ -67,7 +100,7 @@ function questions() {
 
 // function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log("Commit logged!")));
+  fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log(`${fileName} created successfully`)));
 }
 
 // function to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,46 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  const full_name = capitalise(data.full_name);
+  const repo_name = data.repo_name;
+  const project_title = capitalise(data.title);
+  const project_tagline = data.tagline;
+  const project_description = data.description;
+  const email_address = data.email;
+  const github_username = data.github_username;
+  const linkedin_username = data.linkedin_username;
+  const license = data.license;
+  const tests = data.tests;
+
+  const badgeColor = {
+    JavaScript: ["20232A", "FCDD32"],
+    jQuery: ["0769AD", "white"],
+    "Node.js": ["376e05", "white"],
+    React: ["20232A", "61DAFB"],
+    Bootstrap: ["563D7C", "white"],
+    "Express.js": ["404D59", null],
+    "Tailwind-CSS": ["38B2AC", "white"],
+    MongoDB: ["4EA94B", "white"],
+    Netlify: ["00C7B7", "white"],
+    Heroku: ["430098", "white"],
+    HTML5: ["E34F26", "white"],
+    CSS3: ["1572B6", "white"],
+    Sass: ["CC6699", "white"],
+    PHP: ["777BB4", "white"],
+    "Vue.js": ["35495E", "white"],
+    Django: ["092E20", "white"],
+    Laravel: ["FF2D20", "white"],
+    MySQL: ["00000F", "white"],
+  };
+
+  let technologies = data.technologies
+    .map(
+      (technology) =>
+        `![${technology}](https://img.shields.io/badge/${technology}-${badgeColor[technology][0]}?style=for-the-badge&logo=${technology}&logoColor=${badgeColor[technology][1]})\n`
+    )
+    .join("\n");
+
+  let credits = data.credits.map((credit_url) => `- [${credit_url}](${credit_url})`).join("\n");
+
   return `<a name="readme-top"></a>
 
   <!-- PROJECT SHIELDS -->
@@ -8,35 +49,38 @@ function generateMarkdown(data) {
   [![Forks][forks-shield]][forks-url]
   [![Stargazers][stars-shield]][stars-url]
   [![Issues][issues-shield]][issues-url]
-  [![MIT License][license-shield]][license-url]
+  [![${license} License][license-shield]][license-url]
+  [![LinkedIn][linkedin-shield]][linkedin-url]
   
   <!-- PROJECT LOGO -->
+
   <br />
   <div align="center">
-    <a href="https://github.com/${data.username}/${data.repoName}">
-      <img src="assets/images/favicon.png" alt="Logo" width="200" height="200">
+    <a href="https://github.com/${github_username}/${repo_name}">
+      <img src="https://placehold.co/600x400?text=Placeholder" alt="Logo" width="200" height="200">
     </a>
-    <h3 align="center">${data.title}</h3>
+    <h3 align="center">${project_title}</h3>
     <p align="center">
     <br/>
-    ${data.shortDescription}
+    ${project_tagline}
     <br/>
       <br/>
-      ${data.title} is proudly built by: ${data.fullName}
-      <br />
+      ${project_title} is proudly built by: ${full_name}
       <br/>
-      <a href="https://github.com/${data.username}/${data.repoName}"><strong>Explore the docs »</strong></a>
-      <br />
-      <br />
-      <a href="https://${data.username}.github.io/${data.repoName}/">View Live Website</a>
+      <br/>
+      <a href="https://github.com/${github_username}/${repo_name}"><strong>Explore the docs »</strong></a>
+      <br/>
+      <br/>
+      <a href="https://${github_username}.github.io/${repo_name}/">View Live Website</a>
       ·
-      <a href="https://github.com/${data.username}/${data.repoName}/issues">Report Bug</a>
+      <a href="https://github.com/${github_username}/${repo_name}/issues">Report Bug</a>
       ·
-      <a href="https://github.com/${data.username}/${data.repoName}/issues">Request Feature</a>
+      <a href="https://github.com/${github_username}/${repo_name}/issues">Request Feature</a>
     </p>
   </div>
   
   <!-- TABLE OF CONTENTS -->
+
   <details>
     <summary>Table of Contents</summary>
     <ol>
@@ -45,11 +89,14 @@ function generateMarkdown(data) {
         <ul>
           <li><a href="#screenshot">Screenshot</a></li>
           <li><a href="#built-with">Built With</a></li>
-          <li><a href="#user-story">User Story</a></li>
           <li><a href="#description">Description</a></li>
-          <li><a href="#apis">APIs</a></li>
-          <li><a href="#design">Design</a></li>
-          <li><a href="#future-development">Future Development</a></li>
+        </ul>
+      </li>
+      <li>
+          <a href="#getting-started">Getting Started</a>
+        <ul>
+          <li><a href="#prerequisites">Prerequisites</a></li>
+          <li><a href="#installation">Installation</a></li>
         </ul>
       </li>
       <li>
@@ -59,9 +106,10 @@ function generateMarkdown(data) {
           <li><a href="#mobile-responsive">Mobile Responsive</a></li>
         </ul>
       </li>
-      <li><a href="#contributing">Contributing</a></li>
       <li><a href="#license">License</a></li>
-      <li><a href="#contact">Contact</a></li>
+      <li><a href="#contributing">Contributing</a></li>
+      <li><a href="#tests">Tests</a></li>
+      <li><a href="#questions">Questions</a></li>
       <li><a href="#acknowledgments">Acknowledgments</a></li>
     </ol>
   </details>
@@ -72,32 +120,46 @@ function generateMarkdown(data) {
   
   ### Screenshot
   
-  [![Product Name Screen Shot][product-screenshot]](https://${data.username}.github.io/${data.repoName}/)
+  [![Product Name Screen Shot][product-screenshot]](https://${github_username}.github.io/${repo_name}/)
   
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   ### Built With
   
-  - [![${data.technologies}][bootstrap.com]][bootstrap-url]
-  - [![JQuery][jquery.com]][jquery-url]
-  
-  <p align="right"><a href="#readme-top">back to top</a></p>
-  
-  ### User Story
+  ${technologies}
   
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   ### Description
   
-  ${data.longDescription}
+  ${project_description}
   
-  ### APIs
-    
-  ### Design
+  <p align="right"><a href="#readme-top">back to top</a></p>
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
   
-  ### Future Development
+  ### Prerequisites
+
+* npm
+  \`\`\`sh
+  npm install npm@latest -g
+  \`\`\`
+
+### Installation
+
+1. Clone the repo
+   \`\`\`sh
+   git clone https://github.com/${github_username}/${repo_name}.git
+   \`\`\`
+2. Install NPM packages
+   \`\`\`sh
+   npm install
+   \`\`\`
   
-    
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   <!-- USAGE EXAMPLES -->
@@ -108,15 +170,23 @@ function generateMarkdown(data) {
   
   - The following animation demonstrates the applications functionality
   
-    ![Functionality Gif](assets/images/functionality.gif)
-  
+    [![Functionality Gif][functionality-gif]](https://${github_username}.github.io/${repo_name}/)
+
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   ### Mobile Responsive
   
   - As demonstrated in the screenshot below, this application is designed to be responsive and adjust seamlessly to fit various screen sizes
   
-    ![Responsiveness Screenshot](assets/images/responsive-screenshot.png)
+    [![Responsiveness Screenshot][responsive-screenshot]](https://${github_username}.github.io/${repo_name}/)
+  
+  <p align="right"><a href="#readme-top">back to top</a></p>
+
+  <!-- LICENSE -->
+  
+  ## License
+  
+  Distributed under the ${license} License. See \`LICENSE.md\` for more information.
   
   <p align="right"><a href="#readme-top">back to top</a></p>
   
@@ -136,60 +206,68 @@ function generateMarkdown(data) {
   5. Open a Pull Request
   
   <p align="right"><a href="#readme-top">back to top</a></p>
+
+  <!-- TESTS -->
   
-  <!-- LICENSE -->
-  
-  ## License
-  
-  Distributed under the ${data.license} License. See \`LICENSE.md\` for more information.
-  
+  ## Tests
+
+  ${tests}
+    
   <p align="right"><a href="#readme-top">back to top</a></p>
   
-  <!-- CONTACT -->
+  <!-- QUESTIONS -->
   
-  ## Contact
+  ## Questions
+
+  If you have any inquiries, don't hesitate to reach out to me via socials or by sending an email to <a href="mailto:${email_address}">${email_address}</a>
+
+  <a href="https://www.linkedin.com/in/${linkedin_username}/">LinkedIn</a> | <a href="https://github.com/${github_username}/">GitHub</a>
   
-  <a href="https://www.linkedin.com/in/${data.linkedInUsername}/">Michael Yeates</a>
-  
-  Project Link: [https://github.com/${data.username}/${data.repoName}](https://github.com/${data.username}/${data.repoName})
+  Project Link: [https://github.com/${github_username}/${repo_name}](https://github.com/${github_username}/${repo_name})
   
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   <!-- ACKNOWLEDGMENTS -->
   
   ## Acknowledgments
-  
-  - [TomTom Routing API](https://developer.tomtom.com/routing-api/documentation/product-information/introduction)
-  - [TomTom Reverse Geocoding API](https://developer.tomtom.com/reverse-geocoding-api/documentation/product-information/introduction)
-  - [AQI API](https://aqicn.org/api/)
-  - [Leaflet.js](https://leafletjs.com/)
-  - [Jawg Maps](https://www.jawg.io/en/)
-  
+
+  ${credits}
+    
   <p align="right"><a href="#readme-top">back to top</a></p>
   
   <!-- MARKDOWN LINKS & IMAGES -->
-  <!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+  [contributors-shield]: https://img.shields.io/github/contributors/${github_username}/${repo_name}.svg?style=for-the-badge
+  [contributors-url]: https://github.com/${github_username}/${repo_name}/graphs/contributors
   
-  [contributors-shield]: https://img.shields.io/github/contributors/${data.username}/${data.repoName}.svg?style=for-the-badge
-  [contributors-url]: https://github.com/${data.username}/${data.repoName}/graphs/contributors
-  [forks-shield]: https://img.shields.io/github/forks/${data.username}/${data.repoName}.svg?style=for-the-badge
-  [forks-url]: https://github.com/${data.username}/${data.repoName}/network/members
-  [stars-shield]: https://img.shields.io/github/stars/${data.username}/${data.repoName}.svg?style=for-the-badge
-  [stars-url]: https://github.com/${data.username}/${data.repoName}/stargazers
-  [issues-shield]: https://img.shields.io/github/issues/${data.username}/${data.repoName}.svg?style=for-the-badge
-  [issues-url]: https://github.com/${data.username}/${data.repoName}/issues
-  [license-shield]: https://img.shields.io/github/license/${data.username}/${data.repoName}.svg?style=for-the-badge
-  [license-url]: https://github.com/${data.username}/${data.repoName}/blob/master/LICENSE.txt
-  [linkedin-url]: https://linkedin.com/in/${data.linkedInUsername}
-  [product-screenshot]: assets/images/screenshot.png
-  [leafletjs.com]: https://img.shields.io/badge/Leaflet.js-FFFFFF?style=for-the-badge&logo=leaflet&logoColor=green
-  [leaflet-url]: https://leafletjs.com/
-  [bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
-  [bootstrap-url]: https://getbootstrap.com
-  [jquery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
-  [jquery-url]: https://jquery.com
+  [forks-shield]: https://img.shields.io/github/forks/${github_username}/${repo_name}.svg?style=for-the-badge
+  [forks-url]: https://github.com/${github_username}/${repo_name}/network/members
   
+  [stars-shield]: https://img.shields.io/github/stars/${github_username}/${repo_name}.svg?style=for-the-badge
+  [stars-url]: https://github.com/${github_username}/${repo_name}/stargazers
+  
+  [issues-shield]: https://img.shields.io/github/issues/${github_username}/${repo_name}.svg?style=for-the-badge
+  [issues-url]: https://github.com/${github_username}/${repo_name}/issues
+  
+  [license-shield]: https://img.shields.io/github/license/${github_username}/${repo_name}.svg?style=for-the-badge
+  [license-url]: https://github.com/${github_username}/${repo_name}/blob/main/LICENSE
+  
+  [linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+  [linkedin-url]: https://linkedin.com/in/${linkedin_username}
+
+  <!-- UPDATE PLACEHOLDER IMAGES HERE -->
+
+  [product-screenshot]: https://placehold.co/600x400?text=Placeholder
+  [functionality-gif]: https://placehold.co/600x400?text=Placeholder
+  [responsive-screenshot]: https://placehold.co/600x400?text=Placeholder
 `;
+}
+
+function capitalise(str) {
+  return str
+    .split(" ")
+    .map((name) => name[0].toUpperCase() + name.slice(1))
+    .join(" ");
 }
 
 module.exports = generateMarkdown;
